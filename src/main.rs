64fc5df982a67372bfa23d434cf452f95915224f
wartyz <uraniusx@hotmail.com>
2019-04-03 22:45:34 +0200

    video 3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
-// 22:39
+// Video 3    5:22
 use blockchainlib::*;
 
 fn main() {
-    let mut block=Block::new(
+    let difficulty = 0x0000ffff_ffffffff_ffffffff_ffffffff;
+    let mut block = Block::new(
         0,
         0,
-        vec![0;32],
+        vec![0; 32],
         118318,
         "Genesis block!".to_owned(),
-        0x0000ffff_ffffffff_ffffffff_ffffffff);
+        difficulty);
 
-    block.hash=block.hash();
+//    block.hash=block.hash();
+//
+//    println!("{:?}",&block);
 
-    println!("{:?}",&block);
+    block.mine();
 
-    //block.mine();
+    let mut last_hash = block.hash.clone();
 
-    //println!("{:?}",&block);
+    println!("Mined genesis block {:?}", &block);
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            0,
+            last_hash,
+            118318,
+            "Another block".to_owned(),
+            difficulty);
+
+
+        block.mine();
+
+        println!("Mined genesis block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+    }
 }
